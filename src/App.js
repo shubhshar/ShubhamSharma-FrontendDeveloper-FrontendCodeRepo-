25e825a836a3fa57b4328223d8b2749dc656091d
Shubham <49046719+shubhs10@users.noreply.github.com>
2023-03-28 23:27:13 +0530

    Integrated the api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import axios from "axios";
+import { useState,useEffect } from 'react';
 
 function App() {
+
+const [mySpaceData, setMySpaceData]=useState([])
+
+useEffect(()=>{
+  const getData =async()=>{
+    try{
+      const response=await axios.get(`http://localhost:5000/mySpaceXData`)
+      setMySpaceData(response.data)
+      console.log(response.data)
+    }
+    catch(error){
+      setMySpaceData(null)
+    }
+  }
+  getData()
+},[])
   return (
     <div className="App">
-      <h2>spacexdata</h2>
+      {mySpaceData.map((spaceData)=>
+      <div key={spaceData.id}>
+        {spaceData.last_update}
+      </div>
+      )}
     </div>
   );
 }
